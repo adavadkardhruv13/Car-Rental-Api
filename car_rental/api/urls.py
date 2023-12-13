43919a61d05cae6222553e5be3cdf4c47c76e958
Dhruv Adavadkar <adavadkardhruv@gmail.com>
2023-12-13 20:39:48 +0530

    added customer's url

--- car_rental/api/urls.py
+++ car_rental/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import car
+from .views import car, customer
 
 urlpatterns = [
     path('car/', car.view_all_cars),
@@ -8,4 +8,12 @@ urlpatterns = [
     path('car/<int:car_pk>/active_booking/', car.view_car_details_active_booking),
     path('car/<int:car_pk>/update/', car.edit_car_details),
     path('car/<int:car_pk>/delete/', car.delete_car),
+
+    path('', car.home),
+
+    path('customer/', customer.view_all_customers),
+    path('customer/add/', customer.add_customer),
+    path('customer/<int:cust_pk>/', customer.view_customer_details),
+    path('customer/<int:cust_pk>/update/', customer.edit_details),
+    path('customer/<int:cust_pk>/delete/', customer.delete_customer),
 ]
