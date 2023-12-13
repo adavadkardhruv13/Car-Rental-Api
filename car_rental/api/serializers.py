8f76c239f00e96b24f0a967eedb9d5b9b17cebf8
Dhruv Adavadkar <adavadkardhruv@gmail.com>
2023-12-13 20:47:23 +0530

    update serializers

--- car_rental/api/serializers.py
+++ car_rental/api/serializers.py
@@ -1,26 +1,26 @@
 from rest_framework import serializers
 from . models import Car, Customer, Reservation
 
-class CustomerSerializer(serializers.Serializer):
+class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'name', 'email', 'phone']
-class CarSerializer(serializers.Serializer):
+class CarSerializer(serializers.ModelSerializer):
      class Meta:
         model = Car
         fields = ['id', 'vehical_number', 'model', "seating_capacity", 'rent_per_day']
 
 
-class AvailableCarSerializer(serializers.Serializer):
+class AvailableCarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = ['id', 'vehical_number', 'model', "seating_capacity", 'rent_per_day','availability']
 
-class ReservationSerializer(serializers.Serializer):
+class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = ['customer', 'car', 'issue_date', 'return_date']
 
-class CarDetailReservationSerializer(serializers.Serializer):
+class CarDetailReservationSerializer(serializers.ModelSerializer):
     car = CarSerializer()
     current_active_booking = ReservationSerializer(many=True)
